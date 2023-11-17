0678419d483f53c6ddfbed01031de7804380e15d
ricky03knowhere <goose73thecat@gmail.com>
2023-11-17 14:18:58 +0700

    create speech-to-text endpoint

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ from flask import Flask, request, jsonify, make_response, send_from_directory
 import librosa
 import soundfile as sf
 import os
+import io
+import wave
 import numpy as np
+import speech_recognition as sr
 from flask_cors import CORS
 from gtts import gTTS
-
-# from IPython.display import Audio
-# from IPython.display import display
-# from pydub import AudioSegment
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 CORS(app)
@@ -58,11 +58,50 @@ def index():
                 generated_speech, y, sr
             )  # Hasil save dari TTS, dibuka dan ditulis ulang oleh librosa
 
-            # Menjalankan dan menampilkan audio player
-            # play_suara = Audio(generated_speech, autoplay=True)
-            # display(play_suara)
             res = {"fileName": generated_speech, "message": "Succes generated"}
             return make_response(jsonify(res)), 200
         except Exception as err:
             error_message = "An error occurred: " + str(err)
             return make_response(jsonify({"error": error_message}), 500)
+
+@app.route("/api/speech-to-text", methods=["POST"])
+def speech_to_text():
+    recognizer = sr.Recognizer()
+    audio_file = request.files["audio"].read()
+    # print(audio_file)
+    # audio_file = request.files['name']
+    # print('audio_file ==>>', audio_file)
+    # audio_file = io.BytesIO(audio_file)
+    # print("audio_file ==>>", audio_file)
+    # # Save the audio Blob as a file in the uploads folder
+    # filename = secure_filename("uploaded_audio.wav")
+    # file_path = os.path.join("./temp/", filename)
+    file_path = "./temp/tmp.wav"
+    try:
+        with open(file_path, "wb") as f:
+            f.write(audio_file)
+        print(10*'<<===='+'Done')
+    except Exception as err:
+        print("Error =======>>> ", err)
+
+    try:
+        x, _ = librosa.load(file_path, sr=16000)
+        sf.write(file_path, x, 16000)
+        wave.open(file_path, "r")
+        print('audio file =====>>> loaded')
+    except Exception as err:
+        print("Error =======>> ", err)
+
+    with sr.AudioFile(file_path) as source:
+        audio_data = recognizer.record(source)
+    try:
+        text = recognizer.recognize_google(audio_data, language="id-ID")
+        return jsonify({"text": text})
+    except sr.UnknownValueError:
+        print(7 * "=======#" + "Speech Recognition could not understand audio")
+        return jsonify({"error": "Speech Recognition could not understand the audio"}), 400
+    except sr.RequestError as e:
+        print(7 * "=======#" + e)
+        return jsonify({"error": f"Speech Recognition request failed: {e}"}), 500
+    except Exception as err:
+        print("Error ========> ", err)
